7a39ad0551654b5869a2db4caaca0fd0fde58c64
Spareo <dkhenkin@gmail.comgmail.com>
2014-03-23 14:36:05 -0500

    Added test class

---
+++ GTTest.h
@@ -0,0 +1,13 @@
+//
+//  GTTest.h
+//  Git Test
+//
+//  Created by Dmitry Khenkin on 3/23/14.
+//  Copyright (c) 2014 Dmitry Khenkin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTTest : NSObject
+
+@end
---
+++ GTTest.m
@@ -0,0 +1,13 @@
+//
+//  GTTest.m
+//  Git Test
+//
+//  Created by Dmitry Khenkin on 3/23/14.
+//  Copyright (c) 2014 Dmitry Khenkin. All rights reserved.
+//
+
+#import "GTTest.h"
+
+@implementation GTTest
+
+@end
